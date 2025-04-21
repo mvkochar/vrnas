ca8aa839a7c29cc4991dc70373735bd1c839fe3d
sgkochar <sgkochar@gmail.com>
2025-04-21 14:40:15 +0300

    add Partners, Articles and Subscribe blocks to Home page, create Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,49 @@ import './css/Footer.css'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer className='footer'>
+      <div className="footer-bar d-f jc-sb">
+        <div>
+          <div><img src="/images/logo.svg" alt="Logo" /></div>
+          <div className="footer-social d-f">
+            <a href="" className="d-b"><img src="/images/footer-social/1.png" alt="footer-social1" /></a>
+            <a href="" className="d-b"><img src="/images/footer-social/2.png" alt="footer-social2" /></a>
+            <a href="" className="d-b"><img src="/images/footer-social/3.png" alt="footer-social3" /></a>
+            <a href="" className="d-b"><img src="/images/footer-social/4.png" alt="footer-social4" /></a>
+          </div>
+        </div>
+        <div>
+          <h3 className="footer-bl-title">Quicklinks</h3>
+          <ul className="footer-bl-list">
+            <li><a href="">Home</a></li>
+            <li><a href="">Pricing Plan</a></li>
+            <li><a href="">Service</a></li>
+            <li><a href="">Blog</a></li>
+            <li><a href="">Our Team</a></li>
+          </ul>
+        </div>
+        <div>
+          <h3 className="footer-bl-title">Support</h3>
+          <ul className="footer-bl-list">
+            <li><a href="">About Us</a></li>
+            <li><a href="">Contact Us</a></li>
+            <li><a href="">FAQ</a></li>
+            <li><a href="">Tems & Conditions</a></li>
+            <li><a href="">Privacy Policy</a></li>
+          </ul>
+        </div>
+        <div>
+          <h3 className="footer-bl-title">Need Help?</h3>
+          <ul className="footer-bl-list">
+            <li className='footer-address'>Tanjung Sari Street no.48, Pontianak City</li>
+            <li className='footer-email'>Support@VRNas.com</li>
+            <li className='footer-phone'>+123 456 7890</li>
+          </ul>
+        </div>
+      </div>
+      <div className="footer-divider"></div>
+      <p className="footer-copyright body-one">&copy; Copyright 2023, All Rights Reserved</p>
+    </footer>
   )
 }
 
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -123,7 +123,7 @@ const Home = () => {
             }
           </div>
         </div>
-        <div><img src="/images/home-features.png" alt="home-features" /></div>
+        <div><img src="/images/home-features.png" alt="home-features" width={608} /></div>
       </section>
       <section className='home-started'>
         <h4 className="page-bl-above text-center">How to get started</h4>
@@ -213,11 +213,110 @@ const Home = () => {
           <div className="home-pricing-line"></div>
         </div>
         <div className="home-pricing-info">
-           <h4 className="pricing-info-above">Start from</h4>
-           <h3 className="pricing-info-title">$99</h3>
-           <a href="" className="pricing-info-started">Get Started</a>
-           <p className="pricing-info-note">30 Days Moneyback Guarantee</p>
+          <h4 className="pricing-info-above">Start from</h4>
+          <h3 className="pricing-info-title">$99</h3>
+          <a href="" className="pricing-info-started">Get Started</a>
+          <p className="pricing-info-note">30 Days Moneyback Guarantee</p>
         </div>
+      </section>
+      <section className='home-partners'>
+        <h4 className="page-bl-above text-center">Our Trusted Partners</h4>
+        <h2 className="h2-title">Discover the Companies We Work With</h2>
+        <div className="home-partners-img">
+          <img src="/images/home-partners.png" alt="home-partners" />
+        </div>
+      </section>
+      <section className='home-articles'>
+        <div className="home-articles-heading d-f">
+          <div>
+            <h4 className="page-bl-above">Our articles</h4>
+            <h2 className="h2-title">Stay Up-to-Date with Our VR Insights</h2>
+          </div>
+          <a href="" className="home-articles-all">See all</a>
+        </div>
+        <div className="home-aticles-content d-f align-center">
+          <div>
+            <h6 className="home-articles-subtitle">Popular Article</h6>
+            <div className="home-articles-popular">
+              <p className="articles-popular-bage">VR Games</p>
+              <div className="d-f jc-sb align-center">
+                <h5 className="articles-popular-title">Entertainment Goes Virtual: The Rise of VR Gaming</h5>
+                <a href="" className="articles-popular-more">
+                  <svg width="18" height="14" viewBox="0 0 18 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M10.5501 0.75L9.90331 1.48167L15.5923 6.51061H0.666748V7.48717H15.5923L9.90331 12.5161L10.5501 13.2478L17.3334 7.25153V6.74626L10.5501 0.75Z" fill="white" />
+                  </svg>
+                </a>
+              </div>
+            </div>
+            <div className="home-articles-pagination d-f">
+              <div className="pagination-item pagination-item__active"></div>
+              <div className="pagination-item"></div>
+              <div className="pagination-item"></div>
+              <div className="pagination-item"></div>
+            </div>
+          </div>
+          <div className='home-articles-recent'>
+            <h6 className="home-articles-subtitle">Recent Article</h6>
+            <div className="articles-recent-box d-f">
+              <div className="articles-recent-item d-f jc-sb align-center">
+                <div><img src="/images/recent-article1.png" alt="recent-article1" /></div>
+                <div>
+                  <p className="recent-item-badge">VR Education</p>
+                  <h5 className="recent-item-title">
+                    The Future of Education: How VR is Revolutionizing the Classroom
+                  </h5>
+                </div>
+                <a href="" className="recent-item-more">
+                  <svg width="14" height="10" viewBox="0 0 14 10" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M8.24016 0L7.72274 0.585339L12.274 4.60849H0.333496V5.38974H12.274L7.72274 9.41289L8.24016 9.99823L13.6668 5.20122V4.79701L8.24016 0Z" fill="white" />
+                  </svg>
+                </a>
+              </div>
+              <div className="articles-recent-item d-f jc-sb align-center">
+                <div><img src="/images/recent-article2.png" alt="recent-article2" /></div>
+                <div>
+                  <p className="recent-item-badge">VR Architecture</p>
+                  <h5 className="recent-item-title">
+                    Bringing Designs to Life: How VR is Changing Architecture
+                  </h5>
+                </div>
+                <a href="" className="recent-item-more">
+                  <svg width="14" height="10" viewBox="0 0 14 10" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M8.24016 0L7.72274 0.585339L12.274 4.60849H0.333496V5.38974H12.274L7.72274 9.41289L8.24016 9.99823L13.6668 5.20122V4.79701L8.24016 0Z" fill="white" />
+                  </svg>
+                </a>
+              </div>
+              <div className="articles-recent-item d-f jc-sb align-center">
+                <div><img src="/images/recent-article3.png" alt="recent-article3" /></div>
+                <div>
+                  <p className="recent-item-badge">VR Entertainment</p>
+                  <h5 className="recent-item-title">
+                    Making Events Memorable: The Power of VR for Corporate and ...
+                  </h5>
+                </div>
+                <a href="" className="recent-item-more">
+                  <svg width="14" height="10" viewBox="0 0 14 10" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M8.24016 0L7.72274 0.585339L12.274 4.60849H0.333496V5.38974H12.274L7.72274 9.41289L8.24016 9.99823L13.6668 5.20122V4.79701L8.24016 0Z" fill="white" />
+                  </svg>
+                </a>
+              </div>
+            </div>
+          </div>
+
+        </div>
+      </section>
+      <section className='home-subscribe d-f jc-sb align-center'>
+        <h3 className="home-subscribe-title">
+          Subscribe to our newsletter for latest updates
+        </h3>
+        <form action="" className='home-subscribe-fm d-f jc-sb'>
+          <input type="email" name="subscribeEmail" placeholder='Enter your email address' />
+          <button type="button">
+            <svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M12.9569 0.387988C12.6234 0.0457321 12.1299 -0.0817752 11.6697 0.052443L0.938664 3.17302C0.453132 3.3079 0.108991 3.69512 0.0162868 4.18703C-0.0784187 4.68767 0.252383 5.32319 0.68456 5.58894L4.03993 7.65121C4.38407 7.8626 4.82826 7.80958 5.11304 7.52236L8.95528 3.6562C9.14869 3.45487 9.46882 3.45487 9.66224 3.6562C9.85565 3.85082 9.85565 4.16623 9.66224 4.36756L5.81333 8.23438C5.52788 8.52094 5.47452 8.96721 5.68461 9.3135L7.73478 12.7025C7.97488 13.1052 8.38838 13.3333 8.8419 13.3333C8.89525 13.3333 8.95528 13.3333 9.00863 13.3266C9.52885 13.2595 9.94235 12.9038 10.0957 12.4005L13.277 1.68319C13.4171 1.22685 13.2904 0.730245 12.9569 0.387988Z" fill="white" />
+            </svg>
+          </button>
+        </form>
       </section>
     </>
   )
