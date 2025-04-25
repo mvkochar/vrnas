b6d1c6a72c1d877a0a1f9ed1676f831c004f7d5d
sgkochar <sgkochar@gmail.com>
2025-04-25 11:53:03 +0300

    add Faq and Subscribe blocks to Pricing page

--- src/pages/Pricing.tsx
+++ src/pages/Pricing.tsx
@@ -1,4 +1,5 @@
-import { FeaturesItem } from '../components'
+import { FaqItem, FeaturesItem } from '../components'
+import FaqItemsList from '../components/FaqItemsList'
 import FeaturesList from '../components/FeaturesList'
 import './css/Pricing.css'
 
@@ -39,7 +40,7 @@ const Pricing = () => {
               <li>Limited access to architecture and design tools</li>
               <li>No access to exclusive events or promotions</li>
             </ul>
-            <a href="" className="plans-item-get">Get 14 Days Free Trial</a> 
+            <a href="" className="plans-item-get">Get 14 Days Free Trial</a>
           </div>
           <div className="pricing-plans-item">
             <h3 className="plans-item-title">Standard Plan</h3>
@@ -53,7 +54,7 @@ const Pricing = () => {
               <li>Limited access to architecture and design tools</li>
               <li>Access to exclusive events or promotions</li>
             </ul>
-            <a href="" className="plans-item-get">Get 14 Days Free Trial</a> 
+            <a href="" className="plans-item-get">Get 14 Days Free Trial</a>
           </div>
           <div className="pricing-plans-item">
             <h3 className="plans-item-title">Premium Plan</h3>
@@ -67,18 +68,18 @@ const Pricing = () => {
               <li>Access to architecture and design tools</li>
               <li>Access to exclusive events or promotions</li>
             </ul>
-            <a href="" className="plans-item-get">Get 14 Days Free Trial</a> 
+            <a href="" className="plans-item-get">Get 14 Days Free Trial</a>
           </div>
         </div>
       </section>
       <section className='pricing-features d-f'>
-         <div><img src="/images/pricing-features.svg" alt="pricing-features" /></div>
-         <div>
+        <div><img src="/images/pricing-features.svg" alt="pricing-features" /></div>
+        <div>
           <h4 className="page-bl-above">Why choose us</h4>
           <h2 className="h2-title">Why Choose Us for Your VR Needs</h2>
           <div className="pricing-features-box d-f">
             {
-              FeaturesList.map((feature)=> {
+              FeaturesList.map((feature) => {
                 return (
                   <FeaturesItem
                     key={`pricing-features${feature.id}`}
@@ -88,7 +89,41 @@ const Pricing = () => {
               })
             }
           </div>
-         </div>
+        </div>
+      </section>
+      <section className='pricing-faq'>
+        <h4 className="page-bl-above text-center">Faq</h4>
+        <h2 className="h2-title">Frequently Asked Questions</h2>
+        <p className="body-one">
+          At VRNas, we want to make sure that you have all the information 
+          you need to make informed decisions about our VR services. 
+          Here are some of the most common questions we receive:
+        </p>
+        <div className="pricing-faq-box d-f">
+          {
+            FaqItemsList.map((faq)=> {
+              return (
+                <FaqItem
+                  key={`pricing-faq${faq.id}`}
+                  {...faq}
+                />
+              )
+            })
+          }
+        </div>
+      </section>
+      <section className='pricing-subscribe d-f jc-sb align-center'>
+        <h3 className="pricing-subscribe-title">
+          Subscribe to our newsletter for latest updates
+        </h3>
+        <form action="" className='pricing-subscribe-fm d-f jc-sb'>
+          <input type="email" name="subscribeEmail" placeholder='Enter your email address' />
+          <button type="button">
+            <svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M12.9569 0.387988C12.6234 0.0457321 12.1299 -0.0817752 11.6697 0.052443L0.938664 3.17302C0.453132 3.3079 0.108991 3.69512 0.0162868 4.18703C-0.0784187 4.68767 0.252383 5.32319 0.68456 5.58894L4.03993 7.65121C4.38407 7.8626 4.82826 7.80958 5.11304 7.52236L8.95528 3.6562C9.14869 3.45487 9.46882 3.45487 9.66224 3.6562C9.85565 3.85082 9.85565 4.16623 9.66224 4.36756L5.81333 8.23438C5.52788 8.52094 5.47452 8.96721 5.68461 9.3135L7.73478 12.7025C7.97488 13.1052 8.38838 13.3333 8.8419 13.3333C8.89525 13.3333 8.95528 13.3333 9.00863 13.3266C9.52885 13.2595 9.94235 12.9038 10.0957 12.4005L13.277 1.68319C13.4171 1.22685 13.2904 0.730245 12.9569 0.387988Z" fill="white" />
+            </svg>
+          </button>
+        </form>
       </section>
     </>
   )
